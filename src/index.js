41ecf21ba73c5ba43d7aa40ab0bef84ea14f3ac7
Yajas Sardana <yajassardana@gmail.com>
2020-07-02 02:50:26 +0530

    configure word recommendation

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import axios from 'axios';
 const errors = document.querySelector('.errors');
-const resultWord = document.querySelector('.result-word')
+const resultWord = document.querySelector('.result-word');
 const loading = document.querySelector('.loading');
 const synonyms = document.querySelector('.Synonyms'); //deaths
 const definition = document.querySelector('.Definition'); //cases
 const results = document.querySelector('.result-container');
+const didYouMean = document.querySelector('.did-you-mean');
+const noMatch = document.querySelector('.no-match');
 results.style.display = 'none';
 loading.style.display = 'none';
+noMatch.style.display = 'none';
 errors.textContent = '';
+let random = 'key=6bc8b42d-c598-4c95-add4-07268328955f';
 // grab the form
 const form = document.querySelector('.form-data');
 // grab the country name
@@ -17,27 +21,36 @@ const word = document.querySelector('.word');
 const searchForWord = async (word) => {
 	loading.style.display = 'block';
 	errors.textContent = '';
+	results.style.display = 'none';
+	loading.style.display = 'none';
+	noMatch.style.display = 'none';
 	try {
 		const response = await axios.get(
-			`https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?key=6bc8b42d-c598-4c95-add4-07268328955f`
+			`https://dictionaryapi.com/api/v3/references/thesaurus/json/${word}?${random}`
 		);
 		loading.style.display = 'none';
-		definition.textContent = response.data[0].def[0].sseq[0][0][1].dt[0][1];
-		// recovered.textContent = response.data.recovered.value;
-		let synArray="";
-		response.data[0].meta.syns.forEach((syn)=>{
-			syn.forEach((synonym) => {
-				synArray = synArray + synonym + ', '
+
+		if (typeof response.data[0] === 'object') {
+			definition.textContent = response.data[0].def[0].sseq[0][0][1].dt[0][1];
+			let synArray = '';
+			response.data[0].meta.syns.forEach((syn) => {
+				syn.forEach((synonym) => {
+					synArray = synArray + synonym + ', ';
+				});
 			});
-		})
-		synArray = synArray.substring(0,synArray.length-2);
-		synonyms.innerHTML = synArray;
-		resultWord.textContent = word;
-		results.style.display = 'block';
+			synArray = synArray.substring(0, synArray.length - 2);
+			synonyms.innerHTML = synArray;
+			resultWord.textContent = word;
+			results.style.display = 'block';
+		} else if (response.data.length > 0) {
+			didYouMean.textContent = ' ' + response.data[0];
+			noMatch.style.display = 'block';
+		}
 	} catch (error) {
 		loading.style.display = 'none';
 		results.style.display = 'none';
-		errors.textContent = 'Sorry! we have no data for the word you have searched';
+		errors.textContent =
+			'Sorry! we have no data for the word you have searched';
 	}
 };
 
